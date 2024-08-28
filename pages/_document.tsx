75668b897d9a7907350ab94edd714a08b45a5f36
김태근 <oryukdo3@pusan.ac.kr>
2024-08-28 16:08:52 +0900

    feat: meta설정추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<title>함께자라기 | Interactive Study</title>
+				<meta charSet="UTF-8" />
+				<meta
+					name="description"
+					content="함께 배우고 성장하는 상호작용 학습 플랫폼!"
+				/>
+				{/* <meta property="og:title" content="Your default Open Graph Title" />
+				<meta property="og:description"content="Your default Open Graph Description"/> */}
+				{/* <meta property="og:image" content="/images/og-image.png" /> */}
+				{/* <meta property="og:url" content="https://www.yourwebsite.com" /> */}
+				<link rel="icon" href="/app-logo.ico" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
